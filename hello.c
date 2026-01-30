557dc93f9c9f0e183967e60e3228305c9dc8b08d
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-31 01:28:39 +0600

    check extrantion

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
 int main(){
-  printf("Hello world new problem identify and slove");
+  printf("Hello world new problem identify and slove and the best mode nothing to say and  other and more and in fo and the best and  kind  ");
 }
