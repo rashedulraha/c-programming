a083e2b315c9df51bd1954456bb59bb9422f4846
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-01 13:09:22 +0600

    >>

--- hello.c
+++ hello.c
@@ -2,4 +2,4 @@
 
 int main(){
   printf("Hello world new problem identify and slove and the best mode nothing to say and  other and more and in fo and the best and  kind  and how to run this code  and other mind and this is best most and other and more kind and mind be create out own profile and c ");
-}
+};
---
+++ output.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+ int main(){
+
+  int rashed;
+  scanf("%d", &rashed);
+
+  printf("%d" , rashed);
+  return 0;
+ }
