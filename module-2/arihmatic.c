8c845a6c43d1bfb224a2dfe57987b1835bb52336
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-01 18:40:54 +0600

    edit modul one and more

--- module-2/arihmatic.c
+++ module-2/arihmatic.c
@@ -8,11 +8,16 @@
    int c = 3 , d= 10;
 
 
-    float dvd2  =c*1.0/d;
+float dvd2  =c*1.0/d;
 
   //  printf("%d", dvd , dvd2);
-   printf("%f",  dvd2);
+  //  printf("%f",  dvd2);
 
+
+    int firstNumber =5;
+    int secondNumber = 15;
+    float result  = secondNumber *1.0/firstNumber;
+    printf("%f", result);
 
   //  function return
   return 0;
