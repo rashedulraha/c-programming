ba1d3ed70a35d091c3de0e4b5373063d3f82ba6b
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-01 13:37:58 +0600

    scan int and floting value  and print

--- Module-1/main.c
+++ Module-1/main.c
@@ -2,9 +2,27 @@
 
  int main(){
 
+  // input int value
   int firstNumber ,lastNumber;
-  scanf("%d %d", &firstNumber , &lastNumber);
 
-  printf("%d %d" , firstNumber , lastNumber);
+  // input floating value
+  float f;
+
+  // input character value
+  // char c ;
+
+//!scan int value
+  // scanf("%d %d", &firstNumber , &lastNumber);
+
+// scan character  value
+// scanf("%c" , &c);
+scanf("%f" , &f);
+
+
+//! print inting value 
+  // printf("%d %d" , firstNumber , lastNumber);
+
+  //  printf("%c", c);
+  printf("%0.2f" ,f);
   return 0;
  }
