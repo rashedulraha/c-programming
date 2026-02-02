e4f627e23adc3ffb9d0fbbe5034b0271e63ff18d
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-02 09:37:18 +0600

    nested  condition

---
+++ module-2/condition_3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+   int numberOne ;
+
+    scanf("%d",&numberOne);
+
+     if (numberOne >= 50){
+      printf("yes continue");
+
+       if(numberOne >=80){
+        printf("yes number  in more continue");
+       }
+     }
+     else{
+      printf("default continue");
+     }
+
+
+  return 0; 
+}
