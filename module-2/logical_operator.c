84904d3024c769c0e9a77be0adfa42dcd43aa776
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-02 09:19:05 +0600

    conditional statement

---
+++ module-2/logical_operator.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+ int main(){
+   int tk;
+   scanf("%d",&tk);
+
+    if (tk>=100){
+      printf("first continue");
+    }
+    else if (tk >= 50){
+      printf("second continue");
+    }
+    else{
+      printf("default third continue");
+    }
+
+
+
+ return 0;
+ }
