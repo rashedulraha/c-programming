62a889c19106bf17aee6c21981e82de41d95e6f3
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-03 11:56:16 +0600

    problem four

---
+++ module-4/N.Char.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main(){
+  
+ char a ; 
+ scanf("%c",&a);
+
+ if (a >= 'a' && a <= 'z')
+{
+  int ans = a-32;
+  printf("%c",ans);
+
+ }else{
+  int ans = a+32;
+  printf("%c",ans);
+  }
+  return 0;
+}
