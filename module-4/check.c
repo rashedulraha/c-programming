7ddeec1f3ef51bbe0f0e069e64ccc660dcfde514
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-03 09:49:44 +0600

    slove one problem

--- module-4/check.c
+++ module-4/check.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 
- int main(){
+int main(){
 
+   int a , b ;
+   scanf("%d %d" , &a ,&b );
 
+   if (a>=b){
+    printf("Yes\n");
+   }else{
+    printf("No\n");
+   }
 
+   
 
-  return 0;
+   return 0;
  }
