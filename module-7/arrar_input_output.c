c38c34045a58693ef71a36b7d44e44412a1835d4
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-21 17:30:00 +0600

    clear concept  arr input and output in c

--- module-7/arrar_input_output.c
+++ module-7/arrar_input_output.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 
+
 int main(){
-
-  int arr[5];
   
-  for (int i = 0; i < 5; i++){
-    scanf("%d", &arr[i]);
+  int arr[5];
+
+  for (int i=0; i <5 ; i++){
+    scanf("%d",&arr[i]);
   }
 
-  for(int i = 0; i < 5; i++){
-    printf("%d\n", arr[i]);
-  }
 
+   for ( int i = 0; i <5 ; i++){
+    printf("%d\n",arr[i]);
+   }
   return 0;
-}
+} 
