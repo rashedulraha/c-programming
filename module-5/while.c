1c8aa6759fca958e3a7bfa81f8b463c01f1a99c4
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-04-12 05:22:03 +0600

    while

---
+++ module-5/while.c
@@ -0,0 +1,26 @@
+/*
+   start/initialization
+   
+   while(condition){
+   code 
+   step /increment/decrement
+   }
+
+*/
+
+
+#include<stdio.h>
+
+
+int main(){
+  
+  int i  ;
+  i=1;
+  while (i<=5)
+  {
+    printf("%d\n",i);
+    i++;
+  }
+  
+  return 0;
+}
