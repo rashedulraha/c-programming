e537d979bb04933800d486795018fad9a4dcc1c6
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-04-12 05:27:17 +0600

    do while loop

--- module-5/while.c
+++ module-5/while.c
@@ -1,10 +1,40 @@
+// ? while loop
+
 /*
    start/initialization
-   
+
    while(condition){
    code 
    step /increment/decrement
    }
+
+*/
+
+
+// #include<stdio.h>
+
+
+// int main(){
+  
+//   int i  ;
+//   i=1;
+//   while (i<=5)
+//   {
+//     printf("%d\n",i);
+//     i++;
+//   }
+  
+//   return 0;
+// }
+
+
+// ? do while loop
+/*
+start/initialization
+do{
+  code ; 
+  step/increment/decrement
+}while(condition)
 
 */
 
@@ -14,13 +44,14 @@
 
 int main(){
   
-  int i  ;
-  i=1;
-  while (i<=5)
-  {
-    printf("%d\n",i);
-    i++;
-  }
+  int i ;
+  i=0 ;
+  do{
+    printf("%d\n" , i );
+    i=i+1;
+
+  }while (i<=5);
+
   
   return 0;
 }
