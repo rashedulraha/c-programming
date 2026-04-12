5d18c49d290db7f63bcc19777863f9ea6a0932fa
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-04-12 06:12:04 +0600

    problem solve

---
+++ module-5/module-5-1/problem-3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main(){
+  
+  int a ;
+  scanf("%d",&a);
+
+   while(a>=10){
+    a=a/10;
+   }
+
+   if(a%2==0){
+    printf("EVEN");
+   }else{
+    printf("ODD");
+   }
+   
+  return 0;
+}
