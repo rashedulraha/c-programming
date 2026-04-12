45b6f11fcf5c2e7f0c23e2668429261098e9858c
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-04-12 11:44:41 +0600

    capital  small and digit

---
+++ module-5/module-5-1/capital_small_digit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+int main(){
+  
+  char ch ; 
+  scanf("%c", &ch);
+
+  if (ch>='0'&&ch<='9'){
+    printf("IS DIGIT");
+  }else{
+    printf("ALPHA");
+    if (ch >='a'&&ch<='z'){
+      printf("IS SMALL");
+    }else{
+      printf("IS CAPITAL");
+    }
+  }
+
+  return 0;
+}
