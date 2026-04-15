e779451aeb2b8a59fa7236a304ff4e6da7e36e67
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-04-15 18:15:49 +0600

    module 7

--- module-7/01/array.c
+++ module-7/01/array.c
@@ -2,7 +2,22 @@
 
 
 int main(){
+
+  // dic array
+
+  // int array2[5];
+  // array2[1] = 10;
+  // printf("%d",array2[1]);
   
-  printf("%d",45);
+  // printf("%d",45);
+
+  int ar[5];
+  for (int i = 0; i < 5; i++)
+  {
+    scanf("%d" , &ar[i]);
+  }
+
+  printf("%d\n",ar);
+  
   return 0;
 }
