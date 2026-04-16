4ea7ddb3b9312abb292f3af5ad2e19748dc73f9a
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-04-16 11:51:48 +0600

    sum

---
+++ module-7/01/sum_in_arr.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main(){
+  
+  int n ; 
+  scanf("%d",&n);
+
+  int arr[n];
+
+  for(int i = 0; i < n ; i++){
+    scanf("%d", &arr[i]);
+  }
+
+  int sum; 
+
+  for(int i  = 0; i <n ; i++){
+    sum = sum + arr[i];
+  }
+
+  printf("%d" ,sum);
+
+ 
+  return 0;
+}
